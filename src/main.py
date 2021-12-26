b534b99fd6c238c45007a50c62e6b3fb1e631cca
Daniele Mari <daniele.mari@studenti.unipd.it>
2021-12-26 17:56:03 +0100

    [Add]basic cuda setup

---
+++ src/main.py
@@ -0,0 +1,36 @@
+from numba import cuda, vectorize
+from time import time
+import numpy as np
+
+@cuda.reduce
+def sum_reduce(a, b):
+    return a+b
+
+@cuda.jit
+def poly(x):
+    tx = cuda.threadIdx.x
+    ty = cuda.blockIdx.x
+    bw = cuda.blockDim.x
+    pos = ty*bw + tx
+    if pos < x.size:
+        res = 0
+        for i in range(10):
+            res += x[pos] ** i
+        x[pos] = res
+    
+
+if __name__ == "__main__":
+    a = np.random.rand(int(1e8)).astype(np.float64)
+
+    gpu = cuda.get_current_device()
+    max_threads_per_block = gpu.MAX_THREADS_PER_BLOCK
+    max_blocks_per_grid = gpu.MAX_GRID_DIM_X
+    print("Max threads per block:", max_threads_per_block)
+    print("Max blocks per grid:", max_blocks_per_grid)
+    threadsperblock = 1024
+    blockspergrid = min((a.size + (threadsperblock - 1)) // threadsperblock, max_blocks_per_grid)
+    print("threads per block:", threadsperblock)
+    print("blocks per grid:", blockspergrid)
+
+
+
